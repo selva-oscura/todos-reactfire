6e7e920fbee18addfec945683fcf68af8efddf83
Carol St.Louis <1.selva.oscura@gmail.com>
2017-01-19 14:42:23 -0800

    created Title component

---
+++ src/Title.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Title = () => (
+  <div className="row">
+    <h2 className="col s12 title">
+    	So Many Things... <br />
+    	<span className="right">So Little Time...</span>
+    </h2>
+  </div>
+)
+
+module.exports = Title;
