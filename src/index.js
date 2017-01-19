002596ad35c29bb1ffd8705658c2432a46f34fe8
Carol St.Louis <1.selva.oscura@gmail.com>
2017-01-19 14:36:26 -0800

    added firebase config (note: done with stand-in for the specific account information)

--- src/index.js
+++ src/index.js
@@ -2,6 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import * as firebase from "firebase";
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "<API_KEY>",
+  authDomain: "<PROJECT_ID>.firebaseapp.com",
+  databaseURL: "https://<DATABASE_NAME>.firebaseio.com",
+  storageBucket: "<BUCKET>.appspot.com",
+};
+
+firebase.initializeApp(firebaseConfig);
+
 
 ReactDOM.render(
   <App />,
