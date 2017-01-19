04420d09a6dc92dfb02fb3253aacfcd94df3a7d8
Carol St.Louis <1.selva.oscura@gmail.com>
2017-01-19 14:45:36 -0800

    added state, imported and displayed Title, TodoListForm and TodoLists

--- src/App.js
+++ src/App.js
@@ -1,21 +1,98 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Title from './Title';
+import TodoListForm from './TodoListForm';
+import TodoLists from './TodoLists';
+window.listId = 0;
+window.todoId = 0;
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+
+class App extends React.Component{
+  constructor(props){
+    // pass props to parent class
+    super(props);
+    // set initial state
+    this.state = {
+      data: []
+    }
   }
-}
+
+  // add list handler
+  addTodoList(value){
+    // get data
+    const list = {name: value, listId: window.listId++, items:[]}
+    // update data with new list
+    this.state.data.push(list);
+    // update state
+    this.setState({data: this.state.data});
+  }
+
+  // add todo handler
+  addTodo(listId, value){
+    // get data
+    const todo = {text: value, listId: listId, todoId: window.todoId++, checked:false}
+    this.state.data.forEach(function(list, i){
+      // select proper list
+      if(list.listId===listId){
+        // add new todo to that list
+        list.items.push(todo);
+      }
+    });
+    // update data
+    this.setState({data: this.state.data});
+  }
+
+  // handle remove
+  removeTodo(listId, todoId){
+    // get data
+    const data = this.state.data;
+    data.forEach(function(list, i){
+      // select proper list
+      if(list.listId===listId){
+        var updatedList = {
+          name: list.name,
+          listId: list.listId
+        };
+        // Filter all todos except the one to be removed for selected list
+        updatedList.items = list.items.filter((todo) => {
+          if(todo.todoId !== todoId) return todo;
+        });
+        // replace old selected list with updated list
+        data[i] = updatedList;
+      }
+    })
+    // update state with filter
+    this.setState({data: data});
+  }
+  toggleChecked(listId, todoId){
+    // get data
+    var data = this.state.data;
+    data.forEach(function(list){
+      // select proper list
+      if(list.listId===listId){
+        list.items.map((todo) => {
+          // update selected todo in selected list
+          if(todo.todoId===todoId) todo.checked = !todo.checked;
+          return todo;
+        })
+      }
+    })
+    // update data
+    this.setState({data: data});
+  }
+  render(){ 
+    return (
+      <div>
+        <Title />
+        <TodoListForm addTodoList={this.addTodoList.bind(this)} />
+        <TodoLists 
+          todoLists={this.state.data}
+          remove={this.removeTodo.bind(this)}
+          check={this.toggleChecked.bind(this)}
+          addTodo={this.addTodo.bind(this)}
+        />
+      </div>
+    )
+  }
+};
 
 export default App;
